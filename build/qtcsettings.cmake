c509895034b3837e287a57885e2d8317a7db9048
Borys Boiko <burbokop@gmail.com>
2020-08-30 15:22:06 +0300

    debug and build folders removed

--- build/qtcsettings.cmake
+++
@@ -1,7 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "" CACHE "STRING" "Choose the type of build, options are: None(CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug Release RelWithDebInfo MinSizeRel." FORCE)
-set("CMAKE_CXX_COMPILER" "" CACHE "STRING" "No help, variable specified on the command line." FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "" CACHE "STRING" "" FORCE)
--- debug/qtcsettings.cmake
+++
@@ -1,7 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/home/viktor/Qt/5.15.0/gcc_64" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/home/viktor/Qt/5.15.0/gcc_64/bin/qmake" CACHE "STRING" "" FORCE)
